27fada51d846217310a7b3884af081d400310e91
Goutham peddi <peddigoutham93172@gmail.com>
2023-07-02 23:18:04 +0530

    a university event management application

---
+++ src/main/java/org/GouthamPeddi/UniversityEventManagementApplication.java
@@ -0,0 +1,14 @@
+package org.GouthamPeddi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityEventManagementApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(UniversityEventManagementApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/GouthamPeddi/model/Department.java
@@ -0,0 +1,8 @@
+package org.GouthamPeddi.model;
+
+public enum Department {
+    ME,
+    ECE,
+    CIVIL,
+    CSE
+}
